aab57fd2fa13ea9a2baea09fe836d5df1b159c5e
Mikkel Meyer Andersen <mikl@math.aau.dk>
2012-06-25 00:00:00 +0000

    version 1.1

--- R/ddisclap.R
+++ R/ddisclap.R
@@ -1,20 +1,20 @@
 ddisclap <-
-function(d, p) {
+function(x, p) {
   if (any(p <= 0) | any(p >= 1)) stop("0 < p < 1 is required")
   
-  if (length(p) != length(d)) {
+  if (length(p) != length(x)) {
     if (length(p) == 1) {
-      p <- rep(p, length(d))
+      p <- rep(p, length(x))
     } else {
-      stop("length(p) != 1 and length(p) != length(d)")
+      stop("length(p) != 1 and length(p) != length(x)")
     }
   }
   
-  ret <- ((1-p)/(1+p))*p^abs(d) 
+  ret <- ((1-p)/(1+p))*p^abs(x) 
   is <- p == 0
   
   if (length(is) > 0) {
-    ret[is] <- ifelse(d[is] == 0, 1, 0)
+    ret[is] <- ifelse(x[is] == 0, 1, 0)
   }
   
   return(ret)
---
+++ R/rdisclap.R
@@ -0,0 +1,12 @@
+rdisclap <- 
+function(n,p){
+    if(n<0) stop("samplesize must be non negative")
+    n <- floor(n)
+    if (any(p <= 0) | any(p >= 1))
+        stop("0 < p < 1 is required")
+    if (!length(p)%in%c(1,n))
+        stop("length(p) != 1 and length(p) != n")
+    res <- ceiling(log(runif(n)*(1+p)/2)/log(p)-1)
+    sample(c(-1,1),n,replace=TRUE)*res
+}
+
