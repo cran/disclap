fda64495453dcd096819591215ed3e7c7212ef42
Mikkel Meyer Andersen <mikl@math.aau.dk>
2013-05-01 00:00:00 +0000

    version 1.4

--- R/ddisclap.R
+++ R/ddisclap.R
@@ -1,6 +1,6 @@
 ddisclap <-
 function(x, p) {
-  if (any(p <= 0) | any(p >= 1)) stop("0 < p < 1 is required")
+  if (any(p < 0) | any(p >= 1)) stop("0 <= p < 1 is required")
   
   if (length(p) != length(x)) {
     if (length(p) == 1) {
@@ -11,11 +11,6 @@ function(x, p) {
   }
   
   ret <- ((1-p)/(1+p))*p^abs(x) 
-  is <- p == 0
-  
-  if (length(is) > 0) {
-    ret[is] <- ifelse(x[is] == 0, 1, 0)
-  }
   
   return(ret)
 }
--- R/rdisclap.R
+++ R/rdisclap.R
@@ -2,11 +2,12 @@ rdisclap <-
 function(n,p){
     if(n<0) stop("samplesize must be non negative")
     n <- floor(n)
-    if (any(p <= 0) | any(p >= 1))
-        stop("0 < p < 1 is required")
+    if (any(p < 0) | any(p >= 1))
+        stop("0 <= p < 1 is required")
     if (!length(p)%in%c(1,n))
         stop("length(p) != 1 and length(p) != n")
     res <- ceiling(log(runif(n)*(1+p)/2)/log(p)-1)
+    res[p == 0] <- 0
     sample(c(-1,1),n,replace=TRUE)*res
 }
 
